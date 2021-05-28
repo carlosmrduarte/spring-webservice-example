883b53f909b63d5446ec4507c3eefd2d89271f15
Carlos Duarte <c.duarte@kigroup.de>
2021-05-28 15:28:37 +0100

    chore(dependencies): update dependencies and remove unnecessary plugins and dependencies

--- src/test/java/io/xgeekshq/demo/controller/UserControllerTest.java
+++ src/test/java/io/xgeekshq/demo/controller/UserControllerTest.java
@@ -1,6 +1,5 @@
 package io.xgeekshq.demo.controller;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -14,13 +13,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,10 +24,8 @@ import io.xgeekshq.demo.dto.UserDto;
 import io.xgeekshq.demo.model.Gender;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
-@AutoConfigureRestDocs
-public class UserControllerTest {
+class UserControllerTest {
 
     @Autowired
     private MockMvc mvc;
@@ -51,7 +45,7 @@ public class UserControllerTest {
 
         this.mvc.perform(
                 post("/api/user").contentType(MediaType.APPLICATION_JSON).content(jsonMapper.writeValueAsString(dto)))
-                .andExpect(status().isCreated()).andDo(document("create-user"));
+                .andExpect(status().isCreated());
 
         this.mvc.perform(get("/api/users").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
                 .andExpect(jsonPath("$").isNotEmpty()).andExpect(jsonPath("$").value(Matchers.hasSize(2)))
@@ -70,8 +64,7 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.lastName").value("Duarte"))
                 .andExpect(jsonPath("$.email").value("c.duarte@xgeeks.io"))
                 .andExpect(jsonPath("$.gender").value(Gender.MALE.getValue()))
-                .andExpect(jsonPath("$.birthDate").value(LocalDate.of(1992, Month.APRIL, 10).toString()))
-                .andDo(document("user-by-id"));
+                .andExpect(jsonPath("$.birthDate").value(LocalDate.of(1992, Month.APRIL, 10).toString()));
     }
 
     @Test
@@ -82,15 +75,13 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.[0].lastName").value("Duarte"))
                 .andExpect(jsonPath("$.[0].email").value("c.duarte@xgeeks.io"))
                 .andExpect(jsonPath("$.[0].gender").value(Gender.MALE.getValue()))
-                .andExpect(jsonPath("$.[0].birthDate").value(LocalDate.of(1992, Month.APRIL, 10).toString()))
-                .andDo(document("list-users"));
+                .andExpect(jsonPath("$.[0].birthDate").value(LocalDate.of(1992, Month.APRIL, 10).toString()));
     }
 
     @Test
     @Transactional // to rollback changes to the database
     void deleteUser() throws Exception {
-        this.mvc.perform(delete("/api/user/{userId}", 1)).andExpect(status().isNoContent())
-                .andDo(document("delete-user"));
+        this.mvc.perform(delete("/api/user/{userId}", 1)).andExpect(status().isNoContent());
         this.mvc.perform(get("/api/users")).andExpect(status().isOk()).andExpect(jsonPath("$").isEmpty());
     }
 
