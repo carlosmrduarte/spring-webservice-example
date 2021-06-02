f292b17230bea91d09d06751345b7aaac7d3aa55
Carlos Duarte <carlosmrduarte@gmail.com>
2021-06-02 18:09:38 +0100

    delete outdated example files

--- src/main/resources/import.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO User (first_name, last_name, email, birth_date, gender) VALUES('Carlos', 'Duarte', 'c.duarte@xgeeks.io', '1992-04-10', 'M');
--- src/test/java/com/carlosmrduarte/example/ApplicationTests.java
+++ src/test/java/com/carlosmrduarte/example/ApplicationTests.java
@@ -1,10 +1,10 @@
-package io.xgeekshq.demo;
+package com.carlosmrduarte.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {}
